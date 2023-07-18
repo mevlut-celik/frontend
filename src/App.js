f4a56d107b5e3a0b4967c6f8b4d1670c9fef64c9
mevlut celik <mevlutcech@gmail.com>
2023-07-18 20:30:20 +0300

    update

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+
+// DON'T EDIT ABOVE
+
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import { AuthProvider, AuthenticatedRoute } from "./auth/authentication";
+import Login from "./pages/Login";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AuthProvider >
+      <Routes>
+        <Route path="/" element={<Login />} />
+      </Routes>
+    </AuthProvider>
   );
 }
 
---
+++ src/pages/Login.js
@@ -0,0 +1,141 @@
+import React, { useState, useReducer } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import CloseButton from "react-bootstrap/CloseButton";
+import { HOST } from "../constants/host";
+import "./styles/Access.css";
+
+
+import { useAuth } from "../auth/authentication";
+
+function Login(props) {
+  const [loginInput, setLoginInput] = useReducer(
+    (state, newState) => ({ ...state, ...newState }),
+    {
+      credential: "",
+      password: "",
+    }
+  );
+
+  const [credentialEmpty, setCredentialEmpty] = useState(false);
+  const [passwordEmpty, setPasswordEmpty] = useState(false);
+  const [credentialIncorrect, setCredentialIncorrect] = useState(false);
+  const [passwordIncorrect, setPasswordIncorrect] = useState(false);
+
+  var host = HOST;
+  const { saveToken } = useAuth();
+  const navigate = useNavigate();
+
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    /*fetch(`${host}/api/v1/auth/login/`, {
+      method: "POST",
+      body: JSON.stringify(loginInput),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((response) => {
+        props.onSubmitLogIn(response.status);
+        return response.json();
+      })
+      .then((response) => {
+        response.credential
+          ? setCredentialEmpty(true)
+          : setCredentialEmpty(false);
+
+        response.password ? setPasswordEmpty(true) : setPasswordEmpty(false);
+
+        response.credentials &&
+        response.credentials[0] === "Incorrect username or email."
+          ? setCredentialIncorrect(true)
+          : setCredentialIncorrect(false);
+
+        response.credentials &&
+        response.credentials[0] === "Incorrect password."
+          ? setPasswordIncorrect(true)
+          : setPasswordIncorrect(false);
+
+        if (response.token) {
+          saveToken(response.token);
+          navigate("/discover");
+        }
+      })
+      .catch((error) => console.error("Error:", error));*/
+  }
+
+  const handleInput = (event) => {
+    const name = event.target.name;
+    const newValue = event.target.value;
+    setLoginInput({ [name]: newValue });
+  };
+
+  // console.log(responseStatus);
+
+  return (
+    <form className="access" >
+      <div className="access-btn-close">
+        <CloseButton onClick={props.onClickClose} />
+      </div>
+      <div className="access-content">
+        <div className="form-group mt-3">
+          <input
+            type="email"
+            className={`form-control mt-1 + ${
+              credentialEmpty || credentialIncorrect ? "form-control-error" : ""
+            }`}
+            placeholder="Email"
+            name="credential"
+            id="credential"
+            required
+            defaultValue={loginInput.credential}
+            onChange={handleInput}
+          />
+        </div>
+        {credentialEmpty && (
+          <div className="form-error">Please fill in this field.</div>
+        )}
+        {credentialIncorrect && (
+          <div className="form-error">Incorrect email.</div>
+        )}
+        <div className="form-group mt-3">
+          <input
+            type="password"
+            className={`form-control mt-1 + ${
+              passwordEmpty || passwordIncorrect ? "form-control-error" : ""
+            }`}
+            placeholder="Password"
+            name="password"
+            id="password"
+            required
+            defaultValue={loginInput.password}
+            onChange={handleInput}
+          />
+        </div>
+        {passwordEmpty && (
+          <div className="form-error">Please fill in this field.</div>
+        )}
+        {passwordIncorrect && (
+          <div className="form-error">Incorrect password.</div>
+        )}
+        <div className="d-grid gap-2 mt-4">
+          <button
+            type="submit"
+            className="btn btn-primary"
+            onClick={handleSubmit}
+        
+          >
+            Log in
+          </button>
+        </div>
+        <p className="forgot-password text-center mt-3">
+          <Link onClick={props.onClickResPass} className="access-link">
+            
+          </Link>
+        </p>
+      </div>
+    </form>
+  );
+}
+
+export default Login;
